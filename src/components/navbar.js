c08ab9ad7c846b6ac65f5acdc0ef99adebf29e3c
RoySamson-stack <rsam9009@gmail.com>
2021-11-15 19:23:30 +0300

    boilerplate for the project

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NavBar (){
+    return (
+      <div>
+        <nav className="">
+          
+        </nav>
+      </div>
+    );
+  
+}
+
+export default NavBar;
