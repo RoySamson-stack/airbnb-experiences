b1d0a86625fb14be555246515737719863f83601
RoySamson-stack <rsam9009@gmail.com>
2021-11-15 21:56:27 +0300

    making the props for the card section

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,32 @@
 import React from "react";
 import Card from "./cards"
 import katie from "../img/image12.png"
+import star from "../img/star.png"
 
 export default function Cards(){
   return(
     <div className="card-section">
+      <Card 
+      image={katie}
+      icon={star}
+      rating="5.0"
+      title="Katie"
+      price="From $500 per night"
+      />
+       <Card 
+      image={katie}
+      icon={star}
+      rating="5.0"
+      title="Katie"
+      price="From $500 per night"
+      />
+       <Card 
+      image={katie}
+      icon={star}
+      rating="5.0"
+      title="Katie"
+      price="From $500 per night"
+      />
    </div>
   )
 }
--- src/components/cards.js
+++ src/components/cards.js
@@ -1,14 +1,13 @@
 import React from "react"
 
-export default function Card(image, icon, title, price, rating){
+export default function Card({image, icon, title, price, rating}){
   return (
     <div className="card">
       <img src={image} alt="image" className="card-img"/>
      <div className="card-stats">
-       <img src={image} className="card-star"/> 
+       <img src={icon} className="card-star"/> 
        <span className="gray">{rating}</span>
-       <span className="gray">{title}</span>
-       <span>{icon}</span>
+       <span className="gray">{}</span>
      </div>
      <p>{title}</p>
      <p><span className="bold">{price}</span></p>
