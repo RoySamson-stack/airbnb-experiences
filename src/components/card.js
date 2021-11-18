df59b80aa996ef71e75348e1f9e7d41f1f38a45a
RoySamson-stack <rsam9009@gmail.com>
2021-11-18 12:07:28 +0300

    adding a .map to run throught the arrays

--- src/components/card.js
+++ src/components/card.js
@@ -1,40 +1,30 @@
 import React from "react";
 import Card from "./cards"
-import katie from "../img/image12.png"
-import star from "../img/star.png"
-import bike from "../img/mountain-bike.png"
-import wedding from "../img/wedding-photography.png"
+import Data from "./data"
+// import katie from "../img/image12.png"
+// import star from "../img/star.png"
+// import bike from "../img/mountain-bike.png"
+// import wedding from "../img/wedding-photography.png"
 
 export default function Cards(){
+
+
+  const cards = Data.map(item =>{
+    return(
+      <Card 
+      image={item.coverImg}
+      // icon={}
+      rating={item.stats.rating}
+      reviewCount={item.stats.reviewCount}
+      country={item.location}
+      title={item.title}
+      price={item.price}
+      />
+    )
+  })
   return(
     <div className="card-section">
-      <Card 
-      image={katie}
-      icon={star}
-      rating="5.0"
-      reviewCount="10"
-      country="Kenya"
-      title="Katie"
-      price="From $500 per night"
-      />
-       <Card 
-      image={wedding}
-      icon={star}
-      rating="5.0"
-      reviewCount="10"
-      country="Kenya"
-      title="Katie"
-      price="From $500 per night"
-      />
-       <Card 
-      image={bike}
-      icon={star}
-      rating="5.0"
-      reviewCount="10"
-      country="Kenya"
-      title="Katie"
-      price="From ${500} per night"
-      />
+      {cards}
    </div>
   )
 }
